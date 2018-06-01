c1ef5b2d8b79462d99bbff2f68910e2623c6623d
sasa-us <sasa_luu@hotmail.com>
2018-06-01 11:26:36 -0700

    set timeout for whole game

---
+++ find-the-mole/app.js
@@ -0,0 +1,10 @@
+$(document).ready(gameTime)
+function gameTime() {
+    setTimeout(stayTime, 5000);
+}
+
+function stayTime() {
+    console.log('game finish');
+    setTimeout(stayout, 1000);
+}
+
